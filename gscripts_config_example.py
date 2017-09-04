6b541ffa4581c591cb8305daa12602269e554733
Maxim Uvarov <maxim.uvarov@linaro.org>
2017-09-04 15:56:53 +0300

    update gscripts_config_example.py
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- gscripts_config_example.py
+++ gscripts_config_example.py
@@ -2,4 +2,7 @@
 
 print "Using config: %s" %  __name__
 
-gcfg = {'bugz': {"login": "my_login", "pass": "my_password"}}
+gcfg = {'bugz': {"login": "log", "pass": "pass"},
+	'gh': {"login": "log", "pass": "pass"},
+	'imap': {"login": "log", "pass": "pass"},
+	'smtp': {"server": "srv", "login": "pass", "pass": "pass"}}
